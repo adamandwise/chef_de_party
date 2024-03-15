54c52726098d5690925d4b4e169045a3be2e22cd
adamwise <awise.developer@gmail.com>
2024-03-15 01:37:18 -0700

    made a few adjustments to the homepage component, its a work in progress

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {} from 'react-native';
-import HomePage from './src/components/HomePage';
+import HomePage from './src/components/HomePage.tsx';
 
 export default function App() {
   return <HomePage />;
--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import {StyleSheet, View, Text, TouchableOpacity} from 'react-native';
 
 function HomePage(): React.JSX.Element {
   const styles = StyleSheet.create({
@@ -7,17 +7,54 @@ function HomePage(): React.JSX.Element {
       flex: 1,
       justifyContent: 'center',
       alignItems: 'center',
+      backgroundColor: '#FDF5E6',
+      padding: 20,
     },
     text: {
       fontSize: 24,
       fontWeight: 'bold',
     },
+    button: {
+      backgroundColor: 'white',
+      padding: 20,
+      borderRadius: 10,
+      margin: 20,
+      elevation: 2,
+      shadowColor: 'rgba(0, 0, 0, 0.2)',
+      shadowOffset: {
+        width: 0,
+        height: 2,
+      },
+      shadowOpacity: 1,
+      shadowRadius: 2,
+    },
+    buttonText: {
+      fontSize: 20,
+      color: 'black',
+      textAlign: 'center',
+    },
   });
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello, World</Text>
-    </View>
+    <>
+      <View style={styles.container}>
+        <View style={{alignItems: 'center', marginTop: 20}}>
+          <Text style={styles.text}>🎉 Chef de Party 🎉</Text>
+        </View>
+        <View style={{flex: 1, justifyContent: 'center'}}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => alert('Create recipe pressed')}>
+            <Text style={styles.buttonText}>Create Recipe</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => alert('View recipe pressed')}>
+            <Text style={styles.buttonText}>View Recipes</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </>
   );
 }
 
